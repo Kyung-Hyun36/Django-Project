e04b799078c4b8763c0bec6dd418b7ffda09d6b7
KyungHyun Kim <rud03gus06@naver.com>
2023-03-13 00:34:57 +0900

    4장 강의 완성
    강의를 따라서 form과 modelform을 생성하고 modelform으로 데이터 저장

--- second/urls.py
+++ second/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.list, name="list")
+    path('list/', views.list, name="list"),
+    path('create/', views.create, name="create"),
+    path('confirm/', views.confirm, name="confirm"),
 ]

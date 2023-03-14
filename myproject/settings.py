aed88b7efef145384bb7f5a922d1ba5fc556a8ee
KyungHyun Kim <rud03gus06@naver.com>
2023-03-14 10:57:16 +0900

    allowed host update
    myproject -> settings.py에서 ALLOWED_HOSTS에 '..ap-northeast-2.compute.amazonaws.com'추가

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-03u!gnbua&&&gk1k(1s(m*h8@cqw#y=2^r7kfklg*edjaaagyg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
